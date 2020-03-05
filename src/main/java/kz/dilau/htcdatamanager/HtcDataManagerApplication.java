2520144842c2725536789f1b2d3a24cb3b20dbbf
a.tygynbaev <a.tygynbaev@platonus.kz>
2020-03-05 18:37:59 +0600

    move web xml config to main class

--- src/main/java/kz/dilau/htcdatamanager/HtcDataManagerApplication.java
+++ src/main/java/kz/dilau/htcdatamanager/HtcDataManagerApplication.java
@@ -2,10 +2,17 @@ package kz.dilau.htcdatamanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HtcDataManagerApplication {
+public class HtcDataManagerApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(HtcDataManagerApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HtcDataManagerApplication.class);
     }
 }
