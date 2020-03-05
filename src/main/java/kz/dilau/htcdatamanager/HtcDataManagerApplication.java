3498de16f35cc0a65240e81717b7418a64f6c0f2
a.tygynbaev <a.tygynbaev@platonus.kz>
2020-03-05 18:27:44 +0600

    init

---
+++ src/main/java/kz/dilau/htcdatamanager/HtcDataManagerApplication.java
@@ -0,0 +1,11 @@
+package kz.dilau.htcdatamanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtcDataManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HtcDataManagerApplication.class, args);
+    }
+}
---
+++ src/test/java/kz/dilau/htcdatamanager/HtcDataManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.dilau.htcdatamanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtcDataManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
