79d9e5afcdf9ee59811424016c028e742a352493
a.tygynbaev <a.tygynbaev@platonus.kz>
2020-03-06 14:29:59 +0600

    add dict controller

---
+++ src/main/java/kz/dilau/htcdatamanager/web/rest/DictionaryResource.java
@@ -0,0 +1,9 @@
+package kz.dilau.htcdatamanager.web.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dictionaries")
+public class DictionaryResource {
+}
