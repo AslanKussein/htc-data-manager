9504f53958429c019ca21128dada35e709ee0583
a.tygynbayev <x6L9Z@QCNqtO>
2020-04-09 23:23:19 +0600

    add maven-install-plugin

--- src/main/java/kz/dilau/htcdatamanager/domain/dictionary/OperationType.java
+++ src/main/java/kz/dilau/htcdatamanager/domain/dictionary/OperationType.java
@@ -1,6 +1,6 @@
 package kz.dilau.htcdatamanager.domain.dictionary;
 
-import kz.dilau.htcdatamanager.domain.BaseDictionary;
+import kz.dilau.htccommons.domain.BaseSystemDictionary;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -9,5 +9,5 @@ import static kz.dilau.htcdatamanager.config.Constants.TABLE_NAME_PREFIX;
 
 @Entity
 @Table(name = TABLE_NAME_PREFIX + "operation_type")
-public class OperationType extends BaseDictionary<Long> {
+public class OperationType extends BaseSystemDictionary<Long> {
 }
