7b1f039a4d3eb299495ae6df820817f039b304f3
a.tygynbayev <x6L9Z@QCNqtO>
2020-04-13 09:58:45 +0600

    add operationCode field to PossibleReasonForBidding dictionary

--- src/main/java/kz/dilau/htcdatamanager/domain/dictionary/PossibleReasonForBidding.java
+++ src/main/java/kz/dilau/htcdatamanager/domain/dictionary/PossibleReasonForBidding.java
@@ -2,6 +2,7 @@ package kz.dilau.htcdatamanager.domain.dictionary;
 
 import kz.dilau.htcdatamanager.domain.base.BaseDictionary;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -10,4 +11,14 @@ import static kz.dilau.htcdatamanager.config.Constants.TABLE_NAME_PREFIX;
 @Entity
 @Table(name = TABLE_NAME_PREFIX + "possible_reason_for_bidding")
 public class PossibleReasonForBidding extends BaseDictionary {
+    @Column(name = "operation_code")
+    private String operationCode;
+
+    public String getOperationCode() {
+        return operationCode;
+    }
+
+    public void setOperationCode(String operationCode) {
+        this.operationCode = operationCode;
+    }
 }
