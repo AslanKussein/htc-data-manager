0304f013450ac73219f9b461193f5fad122c1e48
a.tygynbayev <x6L9Z@QCNqtO>
2020-04-19 21:03:07 +0600

    change real property entity

--- src/main/java/kz/dilau/htcdatamanager/domain/RealProperty.java
+++ src/main/java/kz/dilau/htcdatamanager/domain/RealProperty.java
@@ -1,7 +1,8 @@
 package kz.dilau.htcdatamanager.domain;
 
 import kz.dilau.htcdatamanager.domain.base.AuditableBaseEntity;
-import kz.dilau.htcdatamanager.domain.dictionary.*;
+import kz.dilau.htcdatamanager.domain.dictionary.ObjectType;
+import kz.dilau.htcdatamanager.domain.dictionary.ResidentialComplex;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,56 +20,40 @@ import static kz.dilau.htcdatamanager.config.Constants.TABLE_NAME_PREFIX;
 @Entity
 @Table(name = TABLE_NAME_PREFIX + "real_property")
 public class RealProperty extends AuditableBaseEntity<String, Long> {
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "object_type_id", referencedColumnName = "id")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "object_type_id", referencedColumnName = "id", nullable = false)
     private ObjectType objectType;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "city_id", referencedColumnName = "id")
-    private City city;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "district_id", referencedColumnName = "id")
-    private District district;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "street_id", referencedColumnName = "id")
-    private Street street;
-
-
     @Column(name = "cadastral_number", unique = true)
     private String cadastralNumber;
-
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "residential_complex_id", referencedColumnName = "id")
-    private ResidentialComplex residentialComplex;
-
-    @OneToOne
-    @MapsId
-    private GeneralCharacteristics generalCharacteristics;
-
     @Column(name = "floor")
     private Integer floor;
     @Column(name = "apartment_number")
     private String apartmentNumber;
     @Column(name = "number_of_rooms")
     private Integer numberOfRooms;
-    @Column(name = "total_area")
-    private Integer totalArea;
-    @Column(name = "living_area")
-    private Integer livingArea;
-    @Column(name = "kitchen_area")
-    private Integer kitchenArea;
+    @Column(name = "total_area", nullable = false)
+    private Double totalArea;
+    @Column(name = "living_area", nullable = false)
+    private Double livingArea;
+    @Column(name = "kitchen_area", nullable = false)
+    private Double kitchenArea;
     @Column(name = "balcony_area")
-    private Integer balconyArea;
-    @Column(name = "number_of_bedrooms")
+    private Double balconyArea;
+    @Column(name = "number_of_bedrooms", nullable = false)
     private Integer numberOfBedrooms;
     @Column(name = "atelier")
     private Boolean atelier;//студия
     @Column(name = "separate_bathroom")
     private Boolean separateBathroom;
+    @OneToOne
+    @MapsId
+    private GeneralCharacteristics generalCharacteristics;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "residential_complex_id", referencedColumnName = "id")
+    private ResidentialComplex residentialComplex;
     @OneToOne(fetch = FetchType.LAZY)
     @MapsId
     private PurchaseInfo purchaseInfo;
-
 
     @ElementCollection
     @CollectionTable(name = "real_property_files", joinColumns = @JoinColumn(name = "id"))
