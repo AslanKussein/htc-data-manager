719fec4a15789064fea186767e287f4da52d7cce
a.tygynbayev <x6L9Z@QCNqtO>
2020-04-21 16:25:07 +0600

    add event error handler

--- src/main/java/kz/dilau/htcdatamanager/component/event/EventResource.java
+++ src/main/java/kz/dilau/htcdatamanager/component/event/EventResource.java
@@ -1,8 +1,11 @@
 package kz.dilau.htcdatamanager.component.event;
 
+import kz.dilau.htcdatamanager.component.common.errors.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
@@ -22,22 +25,43 @@ public class EventResource {
     @GetMapping("/{id}")
     public ResponseEntity<EventDto> getEventById(@RequestHeader(AUTHORIZATION) final String token,
                                                  @PathVariable Long id) {
-        EventDto event = eventManager.getEventById(token, id);
-        return ResponseEntity.ok(event);
+        try {
+            EventDto event = eventManager.getEventById(token, id);
+            return ResponseEntity.ok(event);
+        } catch (NotFoundException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    String.format("Event with id %s not found", id, e)
+            );
+        }
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Void> updateEvent(@RequestHeader(AUTHORIZATION) final String token,
                                             @PathVariable Long id,
                                             @RequestBody EventDto event) {
-        eventManager.updateEvent(token, id, event);
-        return ResponseEntity.noContent().build();
+        try {
+            eventManager.updateEvent(token, id, event);
+            return ResponseEntity.noContent().build();
+        } catch (NotFoundException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    String.format("Provide correct event id: %s", id)
+            );
+        }
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteEventById(@RequestHeader(AUTHORIZATION) final String token,
                                                 @PathVariable Long id) {
-        eventManager.deleteEventById(token, id);
-        return ResponseEntity.noContent().build();
+        try {
+            eventManager.deleteEventById(token, id);
+            return ResponseEntity.noContent().build();
+        } catch (NotFoundException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    String.format("Event with id %s not found", id)
+            );
+        }
     }
 }
