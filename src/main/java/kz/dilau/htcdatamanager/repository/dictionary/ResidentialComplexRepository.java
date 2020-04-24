97c4429e35dd711f478a67217cc9bc296ba68669
d.mukhametgalin <d.muhametgalin@inessoft.kz>
2020-04-24 18:26:32 +0600

    query bug fix

--- src/main/java/kz/dilau/htcdatamanager/repository/dictionary/ResidentialComplexRepository.java
+++ src/main/java/kz/dilau/htcdatamanager/repository/dictionary/ResidentialComplexRepository.java
@@ -2,9 +2,11 @@ package kz.dilau.htcdatamanager.repository.dictionary;
 
 import kz.dilau.htcdatamanager.domain.dictionary.ResidentialComplex;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ResidentialComplexRepository extends JpaRepository<ResidentialComplex, Long> {
+    @Query(value = "select r from ResidentialComplex r where r.removed = false")
     List<ResidentialComplex> findAllByRemovedFalse();
 }
