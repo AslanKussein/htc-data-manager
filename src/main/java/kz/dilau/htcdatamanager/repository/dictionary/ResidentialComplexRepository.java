dbd3a081c55a5fbb93d3fcf59fd9ae6123db85a4
d.mukhametgalin <d.muhametgalin@inessoft.kz>
2020-05-02 15:34:10 +0600

    pageable Residential Complex HTC-54

--- src/main/java/kz/dilau/htcdatamanager/repository/dictionary/ResidentialComplexRepository.java
+++ src/main/java/kz/dilau/htcdatamanager/repository/dictionary/ResidentialComplexRepository.java
@@ -1,12 +1,18 @@
 package kz.dilau.htcdatamanager.repository.dictionary;
 
 import kz.dilau.htcdatamanager.domain.dictionary.ResidentialComplex;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-public interface ResidentialComplexRepository extends JpaRepository<ResidentialComplex, Long> {
+public interface ResidentialComplexRepository extends PagingAndSortingRepository<ResidentialComplex, Long> {
     @Query(value = "select r from ResidentialComplex r where r.isRemoved = false")
     List<ResidentialComplex> findAllByRemovedFalse();
+
+    @Query(value = "select r from ResidentialComplex r where r.isRemoved = false")
+    Page<ResidentialComplex> findPageByRemovedFalse(Pageable pageable);
 }
--- src/main/java/kz/dilau/htcdatamanager/service/ResidentialComplexService.java
+++ src/main/java/kz/dilau/htcdatamanager/service/ResidentialComplexService.java
@@ -1,6 +1,8 @@
 package kz.dilau.htcdatamanager.service;
 
 import kz.dilau.htcdatamanager.web.dto.ResidentialComplexDto;
+import kz.dilau.htcdatamanager.web.dto.common.PageDto;
+import kz.dilau.htcdatamanager.web.dto.common.PageableDto;
 
 import java.util.List;
 
@@ -8,6 +10,8 @@ public interface ResidentialComplexService {
     ResidentialComplexDto getById(Long id);
 
     List<ResidentialComplexDto> getAll();
+
+    PageDto<ResidentialComplexDto> getAllPageable(PageableDto dto);
 
     ResidentialComplexDto save(String token, ResidentialComplexDto dto);
 
