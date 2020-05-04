01b46662defdba3cf83ab48c6a062c9a4984af42
Aslan <kussein93@gmail.com>
2020-05-04 11:25:34 +0600

    Добавил crud по комментариям добавил /api для реста

--- src/main/java/kz/dilau/htcdatamanager/config/Constants.java
+++ src/main/java/kz/dilau/htcdatamanager/config/Constants.java
@@ -13,6 +13,7 @@ public final class Constants {
     public static final String STATUS_HISTORIES_REST_ENDPOINT = "/api/app-status-histories";
     public static final String NOTES_REST_ENDPOINT = "/api/notes";
     public static final String DICTIONARIES_REST_ENDPOINT = "/api/dictionaries";
+    public static final String MORTGAGE_REST_ENDPOINT = "/api/mortgage";
 
     private Constants() {
     }
