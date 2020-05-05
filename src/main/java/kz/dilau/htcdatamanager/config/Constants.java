8a3e4ce4b2b666464a6ebe63e62f82bbe1f5cb06
Aslan <kussein93@gmail.com>
2020-05-05 23:39:30 +0600

    HTC-146

--- src/main/java/kz/dilau/htcdatamanager/config/Constants.java
+++ src/main/java/kz/dilau/htcdatamanager/config/Constants.java
@@ -6,13 +6,14 @@ public final class Constants {
     public static final String DICTIONARY_TABLE_NAME_PREFIX = TABLE_NAME_PREFIX + "dic_";
     public static final String AUTHORIZATION_PREFIX = "Bearer";
     public static final String API_REST_ENDPOINT = "/api";
+    public static final String OPEN_API_REST_ENDPOINT = "/open-api";
     public static final String APPLICATIONS_REST_ENDPOINT = "/api/applications";
     public static final String EVENTS_REST_ENDPOINT = "/api/events";
     public static final String CLIENTS_REST_ENDPOINT = "/api/clients";
     public static final String RESIDENTIAL_COMPLEXES_REST_ENDPOINT = "/api/residential-complexes";
     public static final String STATUS_HISTORIES_REST_ENDPOINT = "/api/app-status-histories";
     public static final String NOTES_REST_ENDPOINT = "/api/notes";
-    public static final String DICTIONARIES_REST_ENDPOINT = "/api/dictionaries";
+    public static final String DICTIONARIES_REST_ENDPOINT = "/open-api/dictionaries";
     public static final String MORTGAGE_REST_ENDPOINT = "/api/mortgage";
 
     private Constants() {
