859ac07fa769b8d44b43cf2b78c76789209dd1af
Aslan <kussein93@gmail.com>
2020-05-10 23:18:49 +0600

    HTC-146

---
+++ src/main/resources/db/migration/postgres/V20201005231601__aslan_alter_application.sql
@@ -0,0 +1,6 @@
+alter table htc_dm_application
+    drop column layout_type;
+alter table htc_dm_application
+    drop column objects_type_market;
+alter table htc_dm_application
+    drop column is_pledged;
