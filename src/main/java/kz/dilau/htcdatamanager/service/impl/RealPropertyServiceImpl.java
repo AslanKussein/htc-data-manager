341c1056c535bae6da51832e4356ae822a4b1d79
d.mukhametgalin <d.muhametgalin@inessoft.kz>
2020-05-19 11:17:34 +0600

    bug fix

--- src/main/java/kz/dilau/htcdatamanager/service/impl/RealPropertyServiceImpl.java
+++ src/main/java/kz/dilau/htcdatamanager/service/impl/RealPropertyServiceImpl.java
@@ -1,91 +1,92 @@
-//package kz.dilau.htcdatamanager.service.impl;
-//
-//import kz.dilau.htcdatamanager.domain.GeneralCharacteristics;
-//import kz.dilau.htcdatamanager.domain.RealProperty;
-//import kz.dilau.htcdatamanager.domain.PurchaseInfo;
-//import kz.dilau.htcdatamanager.domain.dictionary.ParkingType;
-//import kz.dilau.htcdatamanager.domain.dictionary.TypeOfElevator;
-//import kz.dilau.htcdatamanager.domain.enums.RealPropertyFileType;
-//import kz.dilau.htcdatamanager.repository.RealPropertyRepository;
-//import kz.dilau.htcdatamanager.repository.RealPropertyRepository;
-//import kz.dilau.htcdatamanager.service.RealPropertyService;
-//import kz.dilau.htcdatamanager.web.dto.PurchaseInfoDto;
-//import kz.dilau.htcdatamanager.web.dto.RealPropertyDto;
-//import kz.dilau.htcdatamanager.web.dto.client.PurchaseInfoClientDto;
-//import kz.dilau.htcdatamanager.web.dto.client.RealPropertyClientDto;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//import org.springframework.util.CollectionUtils;
-//
-//import java.util.*;
-//import java.util.stream.Collectors;
-//
-//import static java.util.Objects.nonNull;
-//import static kz.dilau.htcdatamanager.util.PeriodUtils.mapToBigDecimalPeriod;
-//import static kz.dilau.htcdatamanager.util.PeriodUtils.mapToIntegerPeriod;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class RealPropertyServiceImpl implements RealPropertyService {
-//    private final RealPropertyRepository realPropertyRepository;
-//
-//    public RealPropertyDto getById(Long id) {
-//        return mapToRealPropertyDto(realPropertyRepository.getOne(id));
-//    }
-//
-//    public List<RealPropertyDto> getAll() {
-//        List<RealProperty> realPropertyList = realPropertyRepository.findAll();
-//        List<RealPropertyDto> realPropertyRequestDtoList = new ArrayList<>();
-//        realPropertyList.forEach(item -> realPropertyRequestDtoList.add(mapToRealPropertyDto(item)));
-//        return realPropertyRequestDtoList;
-//    }
-//
-//    public boolean existsByCadastralNumber(String cadastralNumber) {
-//        return realPropertyRepository.existsByCadastralNumber(cadastralNumber);
-//    }
-//
-//    public void deleteById(Long id) {
-////        realPropertyRepository.deleteById(id);
-//    }
-//
-//    public void update(Long id, RealProperty var0) {
-////        RealProperty var1 = realPropertyRepository.getOne(id);
-////        BeanUtils.copyProperties(var0, var1);
-////        realPropertyRepository.save(var1);
-//    }
-//
-//    public void save(RealProperty realProperty) {
-//        realPropertyRepository.save(realProperty);
-//    }
-//
-//    public void addFilesToProperty(Long propertyId, List<String> photoIds, List<String> housingPlans, List<String> virtualTours) {
-//        RealProperty realProperty = realPropertyRepository.getOne(propertyId);
-////        Map<RealPropertyFileType, Set<String>> filesMap = realProperty.getFilesMap();
-////        if (!CollectionUtils.isEmpty(photoIds)) {
-////            if (filesMap.containsKey(RealPropertyFileType.PHOTO)) {
-////                filesMap.get(RealPropertyFileType.PHOTO).addAll(photoIds);
-////            } else {
-////                filesMap.put(RealPropertyFileType.PHOTO, new HashSet<>(photoIds));
-////            }
-////        }
-////        if (!CollectionUtils.isEmpty(photoIds)) {
-////            if (filesMap.containsKey(RealPropertyFileType.HOUSING_PLAN)) {
-////                filesMap.get(RealPropertyFileType.HOUSING_PLAN).addAll(housingPlans);
-////            } else {
-////                filesMap.put(RealPropertyFileType.HOUSING_PLAN, new HashSet<>(housingPlans));
-////            }
-////        }
-////        if (!CollectionUtils.isEmpty(photoIds)) {
-////            if (filesMap.containsKey(RealPropertyFileType.VIRTUAL_TOUR)) {
-////                filesMap.get(RealPropertyFileType.VIRTUAL_TOUR).addAll(virtualTours);
-////            } else {
-////                filesMap.put(RealPropertyFileType.VIRTUAL_TOUR, new HashSet<>(virtualTours));
-////            }
-////        }
-//        realPropertyRepository.save(realProperty);
-//    }
-//
-//    public RealPropertyDto mapToRealPropertyDto(RealProperty realProperty) {
+package kz.dilau.htcdatamanager.service.impl;
+
+import kz.dilau.htcdatamanager.domain.GeneralCharacteristics;
+import kz.dilau.htcdatamanager.domain.RealProperty;
+import kz.dilau.htcdatamanager.domain.PurchaseInfo;
+import kz.dilau.htcdatamanager.domain.dictionary.ParkingType;
+import kz.dilau.htcdatamanager.domain.dictionary.TypeOfElevator;
+import kz.dilau.htcdatamanager.domain.enums.RealPropertyFileType;
+import kz.dilau.htcdatamanager.domain.old.OldPurchaseInfo;
+import kz.dilau.htcdatamanager.repository.RealPropertyRepository;
+import kz.dilau.htcdatamanager.repository.RealPropertyRepository;
+import kz.dilau.htcdatamanager.service.RealPropertyService;
+import kz.dilau.htcdatamanager.web.dto.PurchaseInfoDto;
+import kz.dilau.htcdatamanager.web.dto.RealPropertyDto;
+import kz.dilau.htcdatamanager.web.dto.client.PurchaseInfoClientDto;
+import kz.dilau.htcdatamanager.web.dto.client.RealPropertyClientDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.Objects.nonNull;
+import static kz.dilau.htcdatamanager.util.PeriodUtils.mapToBigDecimalPeriod;
+import static kz.dilau.htcdatamanager.util.PeriodUtils.mapToIntegerPeriod;
+
+@Service
+@RequiredArgsConstructor
+public class RealPropertyServiceImpl implements RealPropertyService {
+    private final RealPropertyRepository realPropertyRepository;
+
+    public RealPropertyDto getById(Long id) {
+        return mapToRealPropertyDto(realPropertyRepository.getOne(id));
+    }
+
+    public List<RealPropertyDto> getAll() {
+        List<RealProperty> realPropertyList = realPropertyRepository.findAll();
+        List<RealPropertyDto> realPropertyRequestDtoList = new ArrayList<>();
+        realPropertyList.forEach(item -> realPropertyRequestDtoList.add(mapToRealPropertyDto(item)));
+        return realPropertyRequestDtoList;
+    }
+
+    public boolean existsByCadastralNumber(String cadastralNumber) {
+        return realPropertyRepository.existsByCadastralNumber(cadastralNumber);
+    }
+
+    public void deleteById(Long id) {
+//        realPropertyRepository.deleteById(id);
+    }
+
+    public void update(Long id, RealProperty var0) {
+//        RealProperty var1 = realPropertyRepository.getOne(id);
+//        BeanUtils.copyProperties(var0, var1);
+//        realPropertyRepository.save(var1);
+    }
+
+    public void save(RealProperty realProperty) {
+        realPropertyRepository.save(realProperty);
+    }
+
+    public void addFilesToProperty(Long propertyId, List<String> photoIds, List<String> housingPlans, List<String> virtualTours) {
+        RealProperty realProperty = realPropertyRepository.getOne(propertyId);
+//        Map<RealPropertyFileType, Set<String>> filesMap = realProperty.getFilesMap();
+//        if (!CollectionUtils.isEmpty(photoIds)) {
+//            if (filesMap.containsKey(RealPropertyFileType.PHOTO)) {
+//                filesMap.get(RealPropertyFileType.PHOTO).addAll(photoIds);
+//            } else {
+//                filesMap.put(RealPropertyFileType.PHOTO, new HashSet<>(photoIds));
+//            }
+//        }
+//        if (!CollectionUtils.isEmpty(photoIds)) {
+//            if (filesMap.containsKey(RealPropertyFileType.HOUSING_PLAN)) {
+//                filesMap.get(RealPropertyFileType.HOUSING_PLAN).addAll(housingPlans);
+//            } else {
+//                filesMap.put(RealPropertyFileType.HOUSING_PLAN, new HashSet<>(housingPlans));
+//            }
+//        }
+//        if (!CollectionUtils.isEmpty(photoIds)) {
+//            if (filesMap.containsKey(RealPropertyFileType.VIRTUAL_TOUR)) {
+//                filesMap.get(RealPropertyFileType.VIRTUAL_TOUR).addAll(virtualTours);
+//            } else {
+//                filesMap.put(RealPropertyFileType.VIRTUAL_TOUR, new HashSet<>(virtualTours));
+//            }
+//        }
+        realPropertyRepository.save(realProperty);
+    }
+
+    public RealPropertyDto mapToRealPropertyDto(RealProperty realProperty) {
 //        GeneralCharacteristics generalCharacteristics = nonNull(realProperty.getResidentialComplex()) ? realProperty.getResidentialComplex().getGeneralCharacteristics() : realProperty.getGeneralCharacteristics();
 //        return RealPropertyDto.builder()
 //                .objectTypeId(realProperty.getObjectTypeId())
@@ -131,17 +132,18 @@
 //                .latitude(realProperty.getLatitude())
 //                .longitude(realProperty.getLongitude())
 //                .build();
-//    }
-//
-//    public List<String> mapPhotoList(RealProperty realProperty, RealPropertyFileType fileType) {
+        return null;
+    }
+
+    public List<String> mapPhotoList(RealProperty realProperty, RealPropertyFileType fileType) {
 //        Set<String> photos = realProperty.getFilesMap().get(fileType);
 //        if (nonNull(photos)) {
 //            return new ArrayList<>(photos);
 //        }
-//        return null;
-//    }
-//
-//    public PurchaseInfoDto mapToPurchaseInfoDto(OldPurchaseInfo info) {
+        return null;
+    }
+
+    public PurchaseInfoDto mapToPurchaseInfoDto(PurchaseInfo info) {
 //        if (nonNull(info)) {
 //            return PurchaseInfoDto.builder()
 //                    .objectPricePeriod(mapToBigDecimalPeriod(info.getObjectPriceFrom(), info.getObjectPriceTo()))
@@ -157,10 +159,10 @@
 //                    .ceilingHeightPeriod(mapToBigDecimalPeriod(info.getCeilingHeightFrom(), info.getCeilingHeightTo()))
 //                    .build();
 //        }
-//        return null;
-//    }
-//
-//    public RealPropertyClientDto mapToRealPropertyClientDto(RealProperty realProperty) {
+        return null;
+    }
+
+    public RealPropertyClientDto mapToRealPropertyClientDto(RealProperty realProperty) {
 //        OldGeneralCharacteristics generalCharacteristics = nonNull(realProperty.getResidentialComplex()) ? realProperty.getResidentialComplex().getGeneralCharacteristics() : realProperty.getGeneralCharacteristics();
 //        return RealPropertyClientDto.builder()
 //                .totalArea(realProperty.getTotalArea())
@@ -178,8 +180,9 @@
 //                .districtId(generalCharacteristics.getDistrictId())
 //                .streetId(generalCharacteristics.getStreetId())
 //                .build();
-//    }
-//
+        return null;
+    }
+
 //    private PurchaseInfoClientDto mapToPurchaseInfoClientDto(OldPurchaseInfo info) {
 //        if (nonNull(info)) {
 //            return PurchaseInfoClientDto.builder()
@@ -190,4 +193,4 @@
 //        }
 //        return null;
 //    }
-//}
+}
