804c75e975225db4cf7b5d3abdb704b6ec6e6c3b
abzal <a.amanzhol@enbek.kz>
2020-05-24 22:46:19 +0600

    Просмотр объекта

--- src/main/java/kz/dilau/htcdatamanager/web/rest/RealPropertyClientViewResource.java
+++ src/main/java/kz/dilau/htcdatamanager/web/rest/RealPropertyClientViewResource.java
@@ -1,8 +1,10 @@
 package kz.dilau.htcdatamanager.web.rest;
 
 import kz.dilau.htcdatamanager.config.Constants;
+import kz.dilau.htcdatamanager.service.ApplicationService;
 import kz.dilau.htcdatamanager.service.RealPropertyClientService;
 import kz.dilau.htcdatamanager.web.dto.ApplicationClientViewDto;
+import kz.dilau.htcdatamanager.web.dto.ApplicationDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,10 +17,11 @@ import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 @RequestMapping(Constants.REAL_PROPERTY_CLIENT_VIEW_REST_ENDPOINT)
 public class RealPropertyClientViewResource {
     private final RealPropertyClientService realPropertyClientService;
+    private final ApplicationService applicationService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<ApplicationClientViewDto> getById(@PathVariable("id") Long id) {
-        ApplicationClientViewDto result = realPropertyClientService.getById(id);
+    public ResponseEntity<ApplicationDto> getById(@PathVariable("id") Long id) {
+        ApplicationDto result = applicationService.getById(null, id);
         return ResponseEntity.ok(result);
     }
 
