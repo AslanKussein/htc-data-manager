a35758f91e6627659c7d506a0bf925d0cc574268
d.mukhametgalin <d.muhametgalin@inessoft.kz>
2020-05-25 23:35:36 +0600

    bug fix

--- src/main/java/kz/dilau/htcdatamanager/service/dictionary/DictionaryDto.java
+++ src/main/java/kz/dilau/htcdatamanager/service/dictionary/DictionaryDto.java
@@ -1,9 +1,10 @@
 package kz.dilau.htcdatamanager.service.dictionary;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class DictionaryDto<ID> {
