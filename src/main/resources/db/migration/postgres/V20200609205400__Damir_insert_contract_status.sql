83b4a81b6a24512a33457abd69d77380003ed605
d.mukhametgalin <d.muhametgalin@inessoft.kz>
2020-06-09 20:58:41 +0600

    инсерт справочника статусов договора HTC-348

---
+++ src/main/resources/db/migration/postgres/V20200609205400__Damir_insert_contract_status.sql
@@ -0,0 +1,2 @@
+insert into htc_dm_dic_contract_status (name_en, name_kz, name_ru, code, is_removed) values ('Договор сформирован', 'Договор сформирован', 'Договор сформирован', '001001', false);
+insert into htc_dm_dic_contract_status (name_en, name_kz, name_ru, code, is_removed) values ('Формирование договора пропущено', 'Формирование договора пропущено', 'Формирование договора пропущено', '001002', false);
