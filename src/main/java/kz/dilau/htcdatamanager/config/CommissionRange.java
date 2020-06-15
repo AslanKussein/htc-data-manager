e3820d3718af44bef80816999c04a6bfe2ea5e0f
d.mukhametgalin <d.muhametgalin@inessoft.kz>
2020-06-16 00:54:41 +0600

    bug fix HTC-402

--- src/main/java/kz/dilau/htcdatamanager/config/CommissionRange.java
+++ src/main/java/kz/dilau/htcdatamanager/config/CommissionRange.java
@@ -8,7 +8,7 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 public class CommissionRange {
-    private int from;
-    private int to;
+    private long from;
+    private long to;
     private BigDecimal amount;
 }
