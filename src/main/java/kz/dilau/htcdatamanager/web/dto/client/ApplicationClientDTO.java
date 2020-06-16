c73ee221b7963c82e7cabeb393decb218fbb6395
abzal <a.amanzhol@enbek.kz>
2020-06-17 01:02:04 +0600

    метод по созданию заявки из КП

--- src/main/java/kz/dilau/htcdatamanager/web/dto/client/ApplicationClientDTO.java
+++ src/main/java/kz/dilau/htcdatamanager/web/dto/client/ApplicationClientDTO.java
@@ -1,13 +1,11 @@
 package kz.dilau.htcdatamanager.web.dto.client;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import kz.dilau.htcdatamanager.domain.Application;
 import lombok.*;
 
 import javax.validation.constraints.NotNull;
-
 import java.time.ZonedDateTime;
 
 import static java.util.Objects.nonNull;
@@ -19,28 +17,22 @@ import static java.util.Objects.nonNull;
 @NoArgsConstructor
 @ApiModel(value = "Модель заявки для КП", description = "Модель заявки для КП")
 public class ApplicationClientDTO {
+    protected ZonedDateTime createdDate;
     @ApiModelProperty(value = "ID заявки")
     private Long id;
     @ApiModelProperty(value = "ID вида операции", required = true)
     @NotNull(message = "Operation type must not be null")
     private Long operationTypeId;
-
     @ApiModelProperty(value = "ID типа объекта", required = true)
     private Long objectTypeId;
-
     @ApiModelProperty(name = "clientLogin", value = "Логин Клиента")
     private String clientLogin;
-
     @ApiModelProperty(name = "sellDataClientDto", value = "Общая информация о сделке продажи объекта")
     private ApplicationSellDataClientDto sellDataClientDto;
-
     @ApiModelProperty(name = "purchaseInfoClientDto", value = "Модель параметров по операции Покупка ")
     private PurchaseInfoClientDto purchaseInfoClientDto;
-
     @ApiModelProperty(value = "Логин агента, на кого назначена заявка")
     private String agent;
-
-    protected ZonedDateTime createdDate;
 
     public ApplicationClientDTO(Application application) {
         this.setId(application.getId());
