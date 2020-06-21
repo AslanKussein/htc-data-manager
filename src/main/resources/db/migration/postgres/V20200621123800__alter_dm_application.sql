b249d96eb7966d60a6a9704fcb834b1804e8df3d
i.yessimova <indira.y.s@yandex.kz>
2020-06-21 20:42:12 +0600

    add app source dictionary fill app source_id on insert

--- src/main/resources/db/migration/postgres/V20200621123800__alter_dm_application.sql
+++ src/main/resources/db/migration/postgres/V20200621123800__alter_dm_application.sql
@@ -1,7 +1,7 @@
 alter table htc_dm_application add column application_source_id bigint;
 
-create table htc_db_dic_application_source (
-id bigint constraint htc_db_dic_application_source_pkey
+create table htc_dm_dic_application_source (
+id bigint constraint htc_dm_dic_application_source_pkey
       primary key ,
 code varchar(255) not null  constraint uk_app_source_code
       unique,
@@ -14,7 +14,7 @@ is_removed boolean default false not null
 
 alter table htc_dm_application add constraint  htc_dm_application_fk_source foreign key (application_source_id) references htc_dm_dic_application_source (id) ;
 
-COMMENT on table htc_db_dic_application_source is 'Справочник источника (создания) заявки';
+COMMENT on table htc_dm_dic_application_source is 'Справочник источника (создания) заявки';
 
 insert into htc_dm_dic_application_source (id,  code, name_en, name_kz, name_ru, is_removed) values (1, '002001', 'from CRM', 'CRM-дан', 'из CRM', false );
 insert into htc_dm_dic_application_source (id,  code, name_en, name_kz, name_ru, is_removed) values (2, '002002', 'from CA', 'КҚ-дан', 'из КП', false );
