83acf43a95dd191177dfc05f2f28b6e0e4a40b97
d.mukhametgalin <d.muhametgalin@inessoft.kz>
2020-06-30 21:55:14 +0600

    bug fix

--- src/main/resources/db/migration/postgres/V20200630212600__Damir_insert_possible_reason.sql
+++ src/main/resources/db/migration/postgres/V20200630212600__Damir_insert_possible_reason.sql
@@ -1,3 +1,3 @@
 INSERT INTO htc_dm_dic_possible_reason_for_bidding (id, name_en, name_kz, name_ru, operation_code) VALUES (8, 'встреча', 'встреча', 'встреча', '001001');
 
-update htc_dm_dic_possible_reason_for_bidding b set operation_type_id = (select t.id from htc_dm_dic_operation_type t where t.code = b.code);
+update htc_dm_dic_possible_reason_for_bidding b set operation_type_id = (select t.id from htc_dm_dic_operation_type t where t.code = b.operation_type_id);
