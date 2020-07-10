c467c588431fda15cc731f41d65c932ba73fc9d3
d.mukhametgalin <d.muhametgalin@inessoft.kz>
2020-07-10 16:15:09 +0600

    migration bug fix

--- src/main/resources/db/migration/postgres/R__Indira_create_v_payments.sql
+++ src/main/resources/db/migration/postgres/R__Indira_create_v_payments.sql
@@ -1,3 +1,5 @@
+drop view if exists htc_dm_v_payment;
+
 create or replace view htc_dm_v_payment as
 select c.id, c.application_id, null sell_application_id , c.contract_number,  c.commission payed_sum, 6  pay_type_id,
        c.file_guid, c.created_by, c.created_date
